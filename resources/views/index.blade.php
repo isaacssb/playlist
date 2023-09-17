c5301b0f1d3183ab08f791423398db2647e62636
Isaac Barreto <isaacssb123@gmail.com>
2023-09-17 12:04:33 -0300

    Add onePage initial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,29 @@
-socorro
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Salve sua Playlist</title>
+    <link rel="stylesheet"  type="text/css" href="{{ asset('/css/app.css') }}">
+</head>
+<body>
+    <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="mp4/bg.mp4" type="video/mp4">
+    </video>
+    <div class="container">
+        <main>
+            <div class="info">
+                <h1 class="title">SALVE SUAS PLAYLISTS AQUI</h1>
+                <p class="description">Não tenha dor de cabeça</p>
+                <div class="box-1">
+                    <div class="btn btn-one">
+                        <span>CLIQUE AQUI</span>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+
+    <footer>
+        Site criado por Isaac Barreto @isaacssb1
+    </footer>
+</body>
+</html>
